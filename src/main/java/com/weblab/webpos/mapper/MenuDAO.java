b0c3f9fef69a46ac893ff12a29baf0ed8dd362ab
ByeongRyul Cho <ryullulalla@gmail.com>
2021-10-11 18:24:32 +0900

    edit configuration

--- src/main/java/com/weblab/webpos/mapper/MenuDAO.java
+++ src/main/java/com/weblab/webpos/mapper/MenuDAO.java
@@ -1,10 +1,13 @@
 package com.weblab.webpos.mapper;
 
 import org.apache.ibatis.annotations.Select;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-@Service
+@Repository
 public interface MenuDAO {
-    @Select("select * from tab")
-    public String getTime();
+
+    @Select("select sysdate from dual")
+    String getTime();
 }
